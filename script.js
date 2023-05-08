a579ed227ab84d8d69ff2830f8261d765c2c379d
Noah Vedamonickam <noah.vedamonickam@gmail.com>
2023-05-08 22:30:18 +1000

    adjusting key errors

--- script.js
+++ script.js
@@ -5,27 +5,38 @@
 let flatKeys = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
 let sharpKeys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
+let sharps = ['C', 'C#', 'D', 'D#', 'E', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+let flats = ['Db', 'Eb', 'F', 'Gb', 'Ab', 'Bb'];
+
 // Main Function
 function doTranspose() {
+	// collect data from html
 	let oldKey = document.querySelector("#oldKey").value;
 	let newKey = document.querySelector("#newKey").value;
 	let transposed = document.querySelector("#lyrics").value;
 	let newLyrics = document.querySelector("#newLyrics");
 
+	// evaluate keys
 	let allKeys;
-	if (transposed.includes("#")) {
-		allKeys = sharpKeys.slice();
+	if (sharps.includes(oldKey)) {
+		if (flats.includes(newKey)) {
+			alert("Invalid Key!");
+			return;
+		} else {
+			allKeys = sharpKeys.slice();
+		}
 	} else {
-		allKeys = flatKeys.slice();
-	}
-
-	if (!allKeys.includes(oldKey) || !allKeys.includes(newKey)) {
-		alert("Invalid Key!");
-		return;
+		if (sharps.includes(newKey)) {
+			alert("Invalid Key!");
+			return;
+		} else {
+			allKeys = flatKeys.slice();
+		}
 	}
 
 	let diff = allKeys.indexOf(newKey) - allKeys.indexOf(oldKey);
 
+	// begin chord search and change
 	let index = transposed.indexOf("[");
 	let trigger = index - 1;
 	while (trigger < index) {
@@ -47,7 +58,10 @@ function doTranspose() {
 		trigger++;
 	}
 
+	// return transposed 
 	newLyrics.innerHTML = transposed;
+
+	// correct formatting
 	var cardStack = document.getElementById('card-stack');
 	cardStack.prepend(card);
 }
